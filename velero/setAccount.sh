4e1d06ebf7163bc34130728b9d9324f74b2609d4
vinish86 <vinish86@gmail.com>
2019-04-01 17:15:26 +0530

    Update setAccount.sh

--- velero/setAccount.sh
+++ velero/setAccount.sh
@@ -2,6 +2,6 @@ export AZURE_SUBSCRIPTION_ID="azure_subsc"
 export AZURE_TENANT_ID="azure_directory"
 export AZURE_CLIENT_ID="applicationId"
 export AZURE_CLIENT_SECRET="applicationSecretPassword"
-export AZURE_RESOURCE_GROUP="scripttest"
+export AZURE_RESOURCE_GROUP="azure_rg"
 
 kubectl create secret generic cloud-credentials --namespace velero --from-literal AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID} --from-literal AZURE_TENANT_ID=${AZURE_TENANT_ID} --from-literal AZURE_CLIENT_ID=${AZURE_CLIENT_ID} --from-literal AZURE_CLIENT_SECRET=${AZURE_CLIENT_SECRET} --from-literal AZURE_RESOURCE_GROUP=${AZURE_RESOURCE_GROUP}
