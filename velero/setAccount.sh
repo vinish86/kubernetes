b2704ad420caf3ca50aadc344881879bfaac9b22
root <root@master01.lcepj0re31yubbkjd3wg1504lf.dx.internal.cloudapp.net>
2019-04-01 11:42:47 +0000

    velero and helm

---
+++ velero/setAccount.sh
@@ -0,0 +1,7 @@
+export AZURE_SUBSCRIPTION_ID="azure_subsc"
+export AZURE_TENANT_ID="azure_directory"
+export AZURE_CLIENT_ID="applicationId"
+export AZURE_CLIENT_SECRET="applicationSecretPassword"
+export AZURE_RESOURCE_GROUP="scripttest"
+
+kubectl create secret generic cloud-credentials --namespace velero --from-literal AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID} --from-literal AZURE_TENANT_ID=${AZURE_TENANT_ID} --from-literal AZURE_CLIENT_ID=${AZURE_CLIENT_ID} --from-literal AZURE_CLIENT_SECRET=${AZURE_CLIENT_SECRET} --from-literal AZURE_RESOURCE_GROUP=${AZURE_RESOURCE_GROUP}
